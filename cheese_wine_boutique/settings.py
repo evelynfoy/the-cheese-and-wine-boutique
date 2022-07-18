92c141841c5a6d9d5cf3a0782e97939fe84da97e
evelynfoy <evelyn.foy@hotmail.com>
2022-07-18 21:11:20 +0000

    Add header with logo and styling

--- cheese_wine_boutique/settings.py
+++ cheese_wine_boutique/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-65#=vq!)f^nh+3fn54ml*ml!+#zq@7+me!zgrsgo+!$(iqn-rx'
+SECRET_KEY = 'django-insecure-65#=vq!)f^nh+3fn54ml*ml!+#zq@7+me!zgrsgo+!$(iqn-rx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
