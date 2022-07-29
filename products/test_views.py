574203e3e7825afcb3e7a82c9b9a3f2b0bcc1837
evelynfoy <evelyn.foy@hotmail.com>
2022-07-29 08:25:35 +0000

    Add test for product detail view for deal product

--- products/test_views.py
+++ products/test_views.py
@@ -2,7 +2,7 @@
     Contains all the tests views for the products application
 """
 from django.test import TestCase
-from .models import Category, Product, Cheese, Wine
+from .models import Category, Product, Cheese, Wine, Deal
 
 
 class TestViews(TestCase):
@@ -60,17 +60,58 @@ class TestViews(TestCase):
             Product Detail template passing in the product for this product
         """
         category = Category.objects.create(name='wine',
-                                            friendly_name='Wine')
+                                           friendly_name='Wine')
         product = Product.objects.create(category=category,
-                                            sku=1,
-                                            name='MERLOT MOULIN DE GASSAC',
-                                            description="Ripe fruit and spices.",
-                                            price=15.75,
-                                            size='75cl')
+                                         sku=1,
+                                         name='MERLOT MOULIN DE GASSAC',
+                                         description="Ripe fruit and spices.",
+                                         price=15.75,
+                                         size='75cl')
         Wine.objects.create(product=product,
                             origin='French',
                             production_method='Sustainable',
                             grape='Merlot',
                             wine_type='red')
         response = self.client.get(f'/products/{product.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_deal_view(self):
+        """
+            Tests rendering of Product Detail view for a deal product.
+            First creates a test category with a set value of 'Wine'.
+            Then creates a test wine product with specific values.
+            Then creates a test category with a set value of 'Cheese'.
+            Then creates a test cheese product with specific values.
+            Then creates a test deal product with specific values.
+            Then checks that a status of 200 is received on requesting the
+            Product Detail template passing in the product for this deal
+        """
+        category_wine = Category.objects.create(name='wine',
+                                                friendly_name='Wine')
+        product_wine = Product.objects.create(category=category_wine,
+                                              sku=1,
+                                              name='MERLOT MOULIN DE GASSAC',
+                                              description="A wine.",
+                                              price=15.75,
+                                              size='75cl')
+        category_cheese = Category.objects.create(name='cheese',
+                                                  friendly_name='Cheese')
+        product_cheese = Product.objects.create(category=category_cheese,
+                                                sku=1,
+                                                name='CAVANBERT',
+                                                description="A cheese.",
+                                                price=8,
+                                                size='230G')
+        category_deal = Category.objects.create(name='deal',
+                                                friendly_name='Deal')
+        product_deal = Product.objects.create(category=category_deal,
+                                              sku=1,
+                                              name='August Deal',
+                                              description="Wine and cheese",
+                                              price=30,
+                                              size='230G and 75cl')
+        Deal.objects.create(product=product_deal,
+                            product1=product_cheese,
+                            product2=product_wine)
+        response = self.client.get(f'/products/{product_deal.id}/')
         self.assertEqual(response.status_code, 200)
