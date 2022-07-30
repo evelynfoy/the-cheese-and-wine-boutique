4fb6e7f0cbe35bbfe089a64abe5cf1183decb5bc
evelynfoy <evelyn.foy@hotmail.com>
2022-07-30 17:53:41 +0000

    Add running total to navbar

--- basket/contexts.py
+++ basket/contexts.py
@@ -7,9 +7,11 @@ from products.models import Product
 
 def basket_contents(request):
     """
-        Makes item details available to all pages
+        A context processor which reads through the basket items in the
+        request session, calculates the number of items, total and delivery
+        cost which it returns as a context making these details available to
+        all templates.
     """
-
     basket_items = []
     total = 0
     product_count = 0
@@ -27,6 +29,8 @@ def basket_contents(request):
 
     delivery = (total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE))/100
 
+    delivery = (total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)/100)
+
     grand_total = delivery + total
 
     context = {
@@ -36,5 +40,4 @@ def basket_contents(request):
         'delivery': delivery,
         'grand_total': grand_total,
     }
-
     return context
