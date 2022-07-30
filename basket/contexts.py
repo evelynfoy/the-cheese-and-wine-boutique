27a810e955a7a261187f38ec8e4ad00cadbfa89f
evelynfoy <evelyn.foy@hotmail.com>
2022-07-30 16:25:31 +0000

    Add delivery and line total to basket context

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
@@ -8,29 +10,19 @@ def basket_contents(request):
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
-    for item_id, item_data in basket.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            basket_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            for size, quantity in item_data['items_by_size'].items():
-                product = get_object_or_404(Product, pk=item_id)
-                total += quantity * product.price
-                product_count += quantity
-                basket_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+            'line_total': product.price * quantity
+        })
+                
 
-    delivery = 0
+    delivery = (total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE))/100
 
     grand_total = delivery + total
 
