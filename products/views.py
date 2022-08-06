1a651527bc0fdafd319093ae018c0807e60acb66
evelynfoy <evelyn.foy@hotmail.com>
2022-08-06 09:28:50 +0000

    Add view and template for adding a cheese product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 """
 Contains all the main views for the product application
 """
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import Product, Cheese, Wine, Deal
 from .forms import ProductForm, CheeseForm
 
@@ -50,6 +51,23 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add product to the store """
     
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if (form.is_valid()):
+            product = form.save()
+            cheese = Cheese(product=product)
+            cheese_form = CheeseForm(request.POST, request.FILES, instance=cheese)
+            if (cheese_form.is_valid()):
+                cheese_form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           'Failed to add product. Please ensure the form is ' +
+                           'valid.')
+    else:
+        form = ProductForm()
+
     form = ProductForm(request.POST, request.FILES)
     cheese_form = CheeseForm(request.POST, request.FILES)
     template = 'products/add_product.html'
