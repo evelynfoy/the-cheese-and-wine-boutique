f4bc862c6be5363c34ed367b31efe87d07ac008b
evelynfoy <evelyn.foy@hotmail.com>
2022-08-13 18:18:47 +0000

    Add filter functionality on category

--- products/views.py
+++ products/views.py
@@ -14,8 +14,15 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            print(categories)
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
