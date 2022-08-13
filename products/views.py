cbebe237ababe5bf8feb574c3d95bfb86de2238c
evelynfoy <evelyn.foy@hotmail.com>
2022-08-13 17:03:36 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ Contains all the main views for the product application
 """
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import Product, Cheese, Wine, Deal, Category
 from .forms import ProductForm, CheeseForm, WineForm, DealForm
@@ -12,6 +13,17 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
